eb658bfd44b4755dbdd19ae72e0ab96faf045b76
abiyandargie <andargieabiy85@gmail.com>
2024-10-26 18:31:42 -0700

    book store app using react

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://root:root@bookk-store-mern.lvcnj.mongodb.net/?retryWrites=true&w=majority&appName=bookk-store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
